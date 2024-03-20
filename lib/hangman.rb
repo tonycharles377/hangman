ada494536011419ada3c574be62d88797a343ff3
tonycharles377 <tonycharles377@gmail.com>
2024-03-20 21:52:36 +0300

    Create instance methods

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,14 +1,52 @@
 class Hangman
-
+    @@number_of_gueses = 0
     @@words = File.readlines('google-10000-english-no-swears.txt').map!(&:chomp)
 
-    attr_accessor :human, :computer, :word
+    attr_accessor :human, :computer
 
     def initialize
         @human = "player1"
         @computer = "player2"
         @word = rand_word(@@words)
+        @@display = @word.split('').map{|char| char = '_'}
+    end
 
+    
+
+    def guess
+        @@number_of_gueses += 1
+        puts "You have #{15 - @@number_of_gueses} live(s) remaining!\nGuess a letter"
+        @char = gets.chomp
+    end
+
+    def display_correct_guess
+
+        if !game_over?
+            guess()
+            @word.split('').each_with_index do |ichar, i|
+                if ichar == @char
+                    @@display[i] = ichar
+                end
+            end
+            p @@display.join(' ')
+
+            if @word.split('') == @@display
+                puts "You win!"
+            else
+                display_correct_guess()
+            end
+        else
+            puts "Game over! You are out of lives!"
+        end
+    end
+
+    def game_over?
+        @@number_of_gueses > 13
+    end
+
+    #game loop
+    def play
+        display_correct_guess
     end
 
     def rand_word(words)
@@ -24,5 +62,5 @@ class Hangman
 end
 
 testing = Hangman.new
-puts testing.word
+testing.play
 

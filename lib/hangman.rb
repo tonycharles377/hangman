e61298ed92912da46173e425ed1d724f1a290cef
tonycharles377 <tonycharles377@gmail.com>
2024-03-22 11:30:27 +0300

    Make guess case insensitive

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,8 +11,6 @@ class Hangman
         @@display = @word.split('').map{|char| char = '_'}
     end
 
-    
-
     def guess
         @@number_of_gueses += 1
         puts "You have #{15 - @@number_of_gueses} live(s) remaining!\nGuess a letter"
@@ -20,11 +18,10 @@ class Hangman
     end
 
     def display_correct_guess
-
         if !game_over?
             guess()
             @word.split('').each_with_index do |ichar, i|
-                if ichar == @char
+                if ichar == @char.downcase
                     @@display[i] = ichar
                 end
             end
@@ -37,6 +34,7 @@ class Hangman
             end
         else
             puts "Game over! You are out of lives!"
+            puts "The correct word is '#{@word}'"
         end
     end
 
@@ -48,6 +46,8 @@ class Hangman
     def play
         display_correct_guess
     end
+
+    private
 
     def rand_word(words)
         word = words.sample
@@ -63,4 +63,3 @@ end
 
 testing = Hangman.new
 testing.play
-
